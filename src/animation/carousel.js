6937f2b4939a876aa77a5b6f2ce9beb0cb0a0058
Ludmila <ludmila.lyiu@gmail.com>
2021-05-21 22:12:51 -0300

    added carousel functionality + animation

---
+++ src/animation/carousel.js
@@ -0,0 +1,39 @@
+import { gsap } from "gsap/dist/gsap.min.js";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger.min.js";
+gsap.registerPlugin(ScrollTrigger);
+
+// ANIMATE ON LOAD
+function animateContent(title, content) {
+  const tl = gsap.timeline();
+  tl.from(title, { yPercent: 100, duration: 1.5 }).from(
+    content,
+    { xPercent: -100, ease: "elastic.out(1, 1)", duration: 3 },
+    "-=1"
+  );
+}
+export function animateProductsSection(section, content, title) {
+  return gsap.timeline({
+    scrollTrigger: {
+      trigger: section,
+      start: "top bottom",
+      end: "bottom 200px",
+      scrub: 1,
+      onEnter: () => animateContent(title, content),
+    },
+  });
+}
+
+// CAROUSEL
+export function animateSlide(element, value, options) {
+  return gsap.to(element, {
+    x: value,
+    ease: "elastic.out(1, 1)",
+    duration: 1,
+    ...options,
+  });
+}
+
+// DESKTOP SLIDE CAROUSEL
+export function dragSlide(element, value) {
+  return gsap.to(element, { x: -value });
+}
--- src/carousel.js
+++ src/carousel.js
@@ -1,1 +1,68 @@
+import {
+  animateProductsSection,
+  animateSlide,
+  dragSlide,
+} from "./animation/carousel";
 
+const productsSection = document.querySelector("#products-section");
+const productsTitle = document.querySelector("#products-title");
+const carouselContent = document.querySelector("#carousel-content");
+const sliderWrappers = document.querySelectorAll(".carousel__wrapper--large");
+const slides = document.querySelectorAll(".product__wrapper");
+let counter = 0;
+let start;
+let change;
+
+// ANIMATION INTERSECTION OBSERVER ENTER
+animateProductsSection(productsSection, carouselContent, productsTitle);
+
+// MOBILE CAROUSEL
+if (window.matchMedia("(max-width: 1199px)").matches) {
+    const size = slides[0].clientWidth;
+    carouselContent.addEventListener("touchstart", (e) => {
+        start = e.touches[0].clientX;
+    });
+    
+    carouselContent.addEventListener("touchmove", (e) => {
+        let touch = e.touches[0].clientX;
+        change = start - touch;
+    });
+    
+    carouselContent.addEventListener("touchend", () => {
+        if (change > 0) return nextSlide(slides, size);
+    prevSlide (size);
+  });
+} else {
+  // DESKTOP
+  const size = sliderWrappers[0].clientWidth;
+  carouselContent.addEventListener("dragstart", (e) => {
+    start = e.clientX;
+  });
+
+  carouselContent.addEventListener("drag", (e) => {
+    let touch = e.clientX;
+    change = start - touch;
+    dragSlide(carouselContent, change);
+  });
+
+  carouselContent.addEventListener("dragend", (e) => {
+    const end = e.clientX;
+    if (start > end) return nextSlide(sliderWrappers, size, { duration: 0.5 });
+    prevSlide(size, { duration: 0.5 });
+  });
+}
+
+// animations
+function nextSlide(slides, size, options) {
+  if (counter >= slides.length - 1) return;
+  counter++;
+  const movePx = -size * counter + "px";
+  animateSlide(carouselContent, movePx, options);
+}
+
+function prevSlide(size, options) {
+  if (counter <= 0) return;
+  counter--;
+  const movePx = -size * counter + "px";
+  animateSlide(carouselContent, movePx, options);
+}
