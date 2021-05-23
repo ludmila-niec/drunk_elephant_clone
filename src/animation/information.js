05f62a395f6791b14f663b6f34b02ba1fc3c844d
Ludmila <ludmila.lyiu@gmail.com>
2021-05-23 01:42:12 -0300

    animation: added information section + slider functionality

--- src/animation/information.js
+++ src/animation/information.js
@@ -9,6 +9,11 @@ function revealImage(container, colorBlock) {
   tl.add(slideXBox(colorBlock, -100));
 }
 
+function fadeInText(content) {
+  gsap.from(content, { opacity: 0, y: 100, duration: 1, stagger: 0.5 });
+}
+
+// DIVIDER IMAGE
 export function animateDivider(container, colorBlock) {
   gsap.timeline({
     scrollTrigger: {
@@ -20,3 +25,51 @@ export function animateDivider(container, colorBlock) {
     },
   });
 }
+
+// ANIMATE CONTENT
+export function animateContent(trigger, title, blocks) {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: trigger,
+      start: "400px bottom",
+      end: "center center",
+      scrub: 1,
+      snap: {
+        snapTo: "labels",
+        duration: { min: 1, max: 3 },
+        delay: 0.2,
+      },
+    },
+  });
+  tl.addLabel("start")
+    .from(title, { opacity: 0, y: 100, duration: 1 })
+    .addLabel("blocks")
+    .from(blocks, { opacity: 0, y: 100, duration: 1.5, stagger: 1 });
+}
+
+
+// ANIMATE SLIDER SECTION
+export function animateSliderSection(trigger, title, img, content) {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: trigger,
+      start: "top bottom",
+      end: "bottom center",
+      scrub: 1,
+      snap: {
+        snapTo: "labels",
+        duration: { min: 1, max: 3 },
+        delay: 0.2,
+      },
+    },
+  });
+  tl.addLabel("start")
+    .from(title, { opacity: 0, y: 100, duration: 1 })
+    .addLabel("img")
+    .from(img, { opacity: 0, y: 100, duration: 1 }, "-=0.5")
+    .addLabel("slider")
+    .from(content, { opacity: 0, y: 100 }, "-=1");
+}
+
+// SLIDER FUNCTIONALITY
+
--- src/information.js
+++ src/information.js
@@ -1,7 +1,61 @@
-import { animateDivider } from "./animation/information";
+import {
+  animateDivider,
+  animateContent,
+  animateSliderSection,
+} from "./animation/information";
+import { animateSlide } from "./animation/carousel";
 const dividerContainer = document.querySelector("#information-divider");
 const dividerColorBlock = document.querySelector(
   "#information-divider-color-block"
 );
 
+// ANIMATION DIVIDER
 animateDivider(dividerContainer, dividerColorBlock);
+
+// FADE IN CONTENT
+const infoContainer = document.querySelector("#information-section");
+const titleInfo = document.querySelector("#information-title");
+const blockContents = document.querySelectorAll(".information__box");
+animateContent(infoContainer, titleInfo, blockContents);
+// FADE IN SLIDER CONTENT
+const hashtagContent = document.querySelector("#hashtag-trigger");
+const hashtagTitle = document.querySelector("#hashtag-title");
+const hashtagLips = document.querySelector("#hashtag-lips");
+const reviewContainer = document.querySelector("#information-review-container");
+animateSliderSection(
+  hashtagContent,
+  hashtagTitle,
+  hashtagLips,
+  reviewContainer
+);
+
+// SLIDER FUNCTIONALITY
+const arrowLeft = document.querySelector("#info-arrow-left");
+const arrowRight = document.querySelector("#info-arrow-right");
+const reviewWrapper = document.querySelector("#review-wrapper");
+const reviewsList = document.querySelectorAll(".reviews__content");
+let counter = 0;
+const size = reviewWrapper.clientWidth;
+
+arrowLeft.addEventListener("click", () => {
+  console.log("left click");
+  prevSlide();
+});
+arrowRight.addEventListener("click", () => {
+  console.log("right click");
+  nextSlide();
+});
+
+function nextSlide() {
+  if (counter >= reviewsList.length - 1) return;
+  counter++;
+  const movePx = -size * counter + "px";
+  animateSlide(reviewWrapper, movePx);
+}
+
+function prevSlide() {
+  if (counter <= 0) return;
+  counter--;
+  const movePx = -size * counter + "px";
+  animateSlide(reviewWrapper, movePx);
+}
